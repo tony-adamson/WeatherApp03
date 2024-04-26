e71a23baaa157297be7ff7910645636420c74bfe
anton.a <anton.adamson@gmail.com>
2024-04-26 15:14:11 +0600

    Initial Commit

---
+++ WeatherApp03/WeatherApp03App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp03App.swift
+//  WeatherApp03
+//
+//  Created by Антон Адамсон on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
